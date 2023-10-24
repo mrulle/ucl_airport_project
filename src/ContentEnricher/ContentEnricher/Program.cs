dd5b54cd7279f7bce4801c807e8a0ab4e7c08975
RKChrist <roo12ney@gmail.com>
2023-10-24 14:25:59 +0200

    Initial

---
+++ src/ContentEnricher/ContentEnricher/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ContentEnricher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
