45da950ef614c75cc2bac5ea2d6c9cc9da3a7afa
RKChrist <roo12ney@gmail.com>
2023-10-24 17:05:22 +0200

    BookingAPI is also containerized now

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
+#!/bin/bash 
+docker compose down
 docker build -f ./src/BookingApi/Dockerfile -t bookingapi ./src/bookingapi/
 docker build -f ./src/ContentEnricher/Dockerfile -t contentenricher  ./src/contentenricher/
 
 read -p "What Environment would you like ('Production(p, P)') else development: " VAR1
 
 if [ "$VAR1" = "Production" ] || [ "$VAR1" = "p" ] || [ "$VAR1" = "P" ]; then
-    echo "Production"
+    echo "<----------------    Production    ---------------->"
     docker compose --env-file ./.env.prod build
     docker compose --env-file ./.env.prod up -d
 else
-    echo "Development"
+    echo "<----------------    Development    ---------------->"
     docker compose --env-file ./.env build
     docker compose --env-file ./.env up -d
 fi
