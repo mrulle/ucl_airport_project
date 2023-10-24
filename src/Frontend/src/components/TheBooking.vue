ed444d8f313dd6d1b2b7f1731fd154d0499c0dcb
Anders Stræde Bjerregaard Nielsen <don-sbn@hotmail.com>
2023-10-24 16:11:54 +0200

    routing for vue

---
+++ src/Frontend/src/components/TheBooking.vue
@@ -0,0 +1,13 @@
+<!-- <script setup></script> -->
+
+<template>
+  <h1>Hej fra TheBooking.vue</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: deeppink;
+  font-weight: bold;
+  font-size: xx-large;
+}
+</style>
