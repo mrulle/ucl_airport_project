a30adb188c9fecf582f8d0d16bac85372dd0ccb1
michael <michael@rulle.xyz>
2023-10-26 22:27:57 +0200

    modified Booking model to match input to stored procedure, added repo for booking

--- src/BookingApi/BookingApi/Models/BookingModel.cs
+++ src/BookingApi/BookingApi/Models/BookingModel.cs
@@ -2,8 +2,15 @@ namespace BookingApi.Models;
 
 public class BookingModel
 {
+    public BookingModel(string flightId) {
+        FlightId = flightId;
+    }
     public string Email { get; set; } = String.Empty;
-    public string BookingId { get; set; } = String.Empty;
     public int PassportNumber { get; set; }
     public int AddedLuggage { get; set; }
+    public string BookingNumber { get; set; } = Guid.NewGuid().ToString();
+    public string BagageId { get; set; } = Guid.NewGuid().ToString();
+    public string FlightId { get; set; } = String.Empty;
+    public string PassengerId { get; set; } = Guid.NewGuid().ToString();
+    public string InputBookingId {get; set; } = Guid.NewGuid().ToString();
 }
