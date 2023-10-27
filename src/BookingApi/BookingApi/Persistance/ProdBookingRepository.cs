f0e869b25f0c68b4a6b000b8b5828e222888489d
Michael Rulle <michael@rulle.xyz>
2023-10-27 08:58:59 +0200

    started adding code to delete bookings in bookingrepo

--- src/BookingApi/BookingApi/Persistance/ProdBookingRepository.cs
+++ src/BookingApi/BookingApi/Persistance/ProdBookingRepository.cs
@@ -21,7 +21,19 @@ public class ProdBookingRepository : IBookingRepository
 
     public bool Delete(string id)
     {
-        throw new NotImplementedException();
+        var cs = "Host=postgres;Username=postgres;Password=postgres;Database=production";
+        using var con = new NpgsqlConnection(cs);
+        con.Open();
+        var getPassengerSql = $"select passenger_id from bookings";
+        using var getPassengerCmd new NpgsqlCommand(getPassengerSql, con);
+        var passenger_id = getPassengerCmd.ExecuteScalar();
+        Console.WriteLine($"found passenger_id: {passenger_id}");
+        // var sql = $"call sp_insert_booking_data('{item.Email}', {item.PassportNumber}, {item.AddedLuggage}, '{item.BagageId}', '{item.FlightId}', '{item.PassengerId}', '{item.InputBookingId}');";
+        // Console.WriteLine($"attempting this statement:\n{sql}");
+        // using var cmd = new NpgsqlCommand(sql, con);
+        // var rowsAffected = cmd.ExecuteNonQuery();
+        con.Close();
+        return $"{item.InputBookingId}";
     }
 
     public List<BookingModel> GetAll()
